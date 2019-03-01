8d771e648c05f17a7992d700e41694189b5064fa
wangyeming <1398738509@qq.com>
2019-03-01 19:40:09 +0800

    update

---
+++ 剑指offer/JAVA_3.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class JAVA_3 {
+
+    public static void main(String[] argv) {
+        int[] data = new int[]{1, 2, 3, 4, 5, 7, 8, 8, 9, 1, 2, 3, 4, 6, 6};
+        List<Integer> results = findAllRepeatNum(data);
+        System.out.println(results);
+    }
+
+    public static List<Integer> findAllRepeatNum(int[] array) {
+        List<Integer> repeatNumArray = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == i) {
+                continue;
+            }
+            while (true) {
+                if (array[i] != i) {
+                    if (array[i] == array[array[i]]) {
+                        repeatNumArray.add(array[i]);
+                        break;
+                    } else {
+                        swap(array, i, array[i]);
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+        return repeatNumArray;
+    }
+
+    public static void swap(int[] array, int i, int j) {
+        int tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
+    }
+}
