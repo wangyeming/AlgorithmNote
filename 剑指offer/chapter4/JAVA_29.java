012ba810425b7d5db6853fdee44ed75f069de194
wangyeming <1398738509@qq.com>
2019-04-04 17:45:42 +0800

    优化代码

--- 剑指offer/chapter4/JAVA_29.java
+++ 剑指offer/chapter4/JAVA_29.java
@@ -28,48 +28,23 @@ public class JAVA_29 {
         int columns = matrix[0].length;
         int rows = matrix.length;
         //圈数
-        int start = 0;
-        while (columns > start * 2 && rows > start * 2) {
-            printCircle(matrix, start);
-            start++;
-        }
+        for (int start = 0; start * 2 < columns && start * 2 < rows; start++) printCircle(matrix, start);
     }
 
     //打印一圈，最后一圈可能不完整，可能只有一边，两边，三边
-    public static void printCircle(int[][] matrix, int start) {
-        int rows = matrix.length;
-        int columns = matrix[0].length;
-
+    private static void printCircle(int[][] matrix, int start) {
+        int rows = matrix.length, columns = matrix[0].length;
         //此时起点的坐标是(start, start)
         //每转一圈，这一圈最终结束的行和列分别需要减去start
-        int endRow = rows - 1 - start;
-        int endColumn = columns - 1 - start;
+        int endRow = rows - 1 - start, endColumn = columns - 1 - start;
 
         //从左向右  →   (start, start)  ->  (start, endColumn)
-        for (int i = start; i <= endColumn; i++) {
-            System.out.println(matrix[start][i]);
-        }
-
-
+        for (int i = start; i <= endColumn; i++) System.out.print(matrix[start][i] + "\t");
         //从上向下  ↓   (start+1, endColumn) -> (endRow, endColumn)
-        if (start < endRow) {
-            for (int i = start + 1; i <= endRow; i++) {
-                System.out.println(matrix[i][endColumn]);
-            }
-        }
-
+        for (int i = start + 1; i <= endRow; i++) System.out.print(matrix[i][endColumn] + "\t");
         //从右向左  ←   (endRow, endColumn-1) -> (endRow, start)
-        if (start < endColumn && start < endRow) {
-            for (int i = endColumn - 1; i >= start; i--) {
-                System.out.println(matrix[endRow][i]);
-            }
-        }
-
+        for (int i = endColumn - 1; i >= start; i--) System.out.print(matrix[endRow][i] + "\t");
         //从下向上   ↑  (endRow - 1, start) -> (start + 1, start)
-        if (start < endRow && start < endColumn - 1) {
-            for (int i = endRow - 1; i >= start + 1; i--) {
-                System.out.println(matrix[i][start]);
-            }
-        }
+        for (int i = endRow - 1; i >= start + 1; i--) System.out.print(matrix[i][start] + "\t");
     }
 }
